2fb66e8a92a10ded0922343081fe672cda8cb119
Luis Arce <luis.arce22@gmail.com>
2017-10-18 13:20:24 -0400

    Updated home page to improve the item showing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  items: Array<{name: string, inStock: string, icon: string}>;
   constructor(public navCtrl: NavController) {
+  	this.items = [];
+    this.items.push({
+        name: 'TV Samsung',
+        inStock: '450',
+        icon: "http://www.lg.com/us/images/tvs/md05791090/thumb-350x350c.jpg"
+      });
+    
+    this.items.push({
+        name: 'TV LG',
+        inStock: '300',
+        icon: "http://brain-images.cdn.dixons.com/8/4/10141248/l_10141248_001.jpg"
+      });
 
   }
 
