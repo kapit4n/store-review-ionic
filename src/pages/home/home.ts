1156b8d87ea2951b9c6a077783a934cb4b92f266
Luis Arce <luis.arce22@gmail.com>
2017-10-19 15:45:44 -0400

    Adding item description

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,33 +6,38 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  items: Array<{name: string, inStock: string, icon: string}>;
+  items: Array<{name: string, inStock: string, icon: string, description: string}>;
   selected: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.selected = navParams.get('item');
   	this.items = [];
+    let description = "Inventory (American English) or stock (British English) is the goods and materials that a business holds for the ultimate goals to have a purpose of resale (or repair)"
     this.items.push({
         name: 'TV Samsung',
         inStock: '450',
-        icon: "assets/img/tv_samsung.jpg"
+        icon: "assets/img/tv_samsung.jpg",
+        description: description
       });
     
     this.items.push({
         name: 'TV LG',
         inStock: '300',
-        icon: "assets/img/tv_lg.jpg"
+        icon: "assets/img/tv_lg.jpg",
+        description: description
       });
     
     this.items.push({
         name: 'Jeep',
         inStock: '5',
-        icon: "assets/img/jeep.jpg"
+        icon: "assets/img/jeep.jpg",
+        description: description
       });
     
     this.items.push({
         name: 'Imac',
         inStock: '5',
-        icon: "assets/img/imac.jpg"
+        icon: "assets/img/imac.jpg",
+        description: description
       });
   }
 
